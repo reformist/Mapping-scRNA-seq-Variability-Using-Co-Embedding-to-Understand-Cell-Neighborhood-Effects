17f3d1391da2b5438daa9f3cb60cc4242449b321
BarRozenman <45449223+BarRozenman@users.noreply.github.com>
2024-10-30 17:12:00 -0400

    separate maxfuse embed and VAE embed

--- visualization/pipeline_real_data.py
+++ visualization/pipeline_real_data.py
@@ -9,6 +9,7 @@ dot.attr(dpi='300')  # Set high DPI for better resolution
 process_attrs = {'shape': 'box', 'style': 'filled', 'fillcolor': 'lightblue'}
 data_attrs = {'shape': 'ellipse', 'style': 'filled', 'fillcolor': 'lightgrey'}
 embedding_attrs = {'shape': 'diamond', 'style': 'filled', 'fillcolor': 'lightyellow'}
+embedding_attrs_2 = {'shape': 'diamond', 'style': 'filled', 'fillcolor': 'yellow'}
 model_attrs = {'shape': 'hexagon', 'style': 'filled', 'fillcolor': 'lightgreen'}
 
 # Data Generation Step
@@ -18,38 +19,46 @@ dot.node('cn_data', 'CN (Cell Neighborhoods)', **data_attrs)
 
 # MaxFuse Co-embedding Step
 dot.node('maxfuse', 'Train MaxFuse Co-embedding', **process_attrs)
-dot.node('co_embedding', 'Co-embedding Space', **embedding_attrs)
+dot.node('co_embedding', 'Protein Co-embedding Space', **embedding_attrs)
+dot.node('co_embedding_2', 'RNA Co-embedding Space', **embedding_attrs)
 
 # Arrows for MaxFuse Step with minimum lengths and label distance
 dot.edge('rna_data', 'maxfuse', label='',  minlen='2')
-dot.edge('maxfuse', 'cn_pred', label=' RNA Co-Embeddings\nOutputs', minlen='2', labeldistance='2')
+# dot.edge('maxfuse', 'vae_contrastive_2', label=' RNA Co-Embeddings\nOutputs', minlen='2', labeldistance='2')
+dot.edge('maxfuse', 'co_embedding_2', label=' RNA Co-Embeddings\nOutputs', minlen='2', labeldistance='2')
+dot.edge('co_embedding_2', 'vae_contrastive_2', label=' RNA Co-Embeddings\nOutputs', minlen='2', labeldistance='2')
 dot.edge('protein_data', 'maxfuse', label='',  minlen='2')
 dot.edge('protein_data', 'cn_data', label=' Calculate CN', minlen='2', labeldistance='2')
 dot.edge('maxfuse', 'co_embedding', label=' Protein Co-Embeddings Outputs', minlen='2', labeldistance='2')
 
 # VAE with Integrated Contrastive Learning
 dot.node('vae_contrastive', 'VAE with Integrated\nContrastive Learning', **model_attrs)
+dot.node('vae_contrastive_2', 'VAE with Integrated\nContrastive Learning', **model_attrs)
+dot.node('latent_space', 'CN Aware RNA Latent Space -\nCan Be Used For Classification tasks', **embedding_attrs_2)
+
 
 # Plate for Latent Space and Contrastive Learning
 with dot.subgraph(name='cluster_plate') as plate:
-    plate.attr(label='VAE with \nContrastive Learning', color='black', style='dashed')
-    plate.node('latent_space', 'Latent Space', **embedding_attrs)
+    plate.attr(label='Train VAE with \nContrastive Learning', color='black', style='dashed')
     # plate.node('contrastive', 'Contrastive Loss\non CN and Cell Type', **process_attrs)
 
     # Edges within the Plate with added minlen and labeldistance for clarity
     plate.edge('co_embedding', 'vae_contrastive', label='Input Co-embedding', minlen='2', labeldistance='2')
-    plate.edge('vae_contrastive', 'latent_space', label='Encodes to', minlen='2', labeldistance='2')
-    # plate.edge('contrastive', 'latent_space', label='Refines', minlen='2', labeldistance='2')
-
+    plate.edge('vae_contrastive', 'latent_space_2', label='Encode To', minlen='2', labeldistance='2')
     # Invisible node for plate connection and forcing bottom placement
     plate.node('plate_rep', '', shape='point')
-    plate.edge('latent_space', 'plate_rep', style='invis')  # Invisible edge to control rank
+    plate.node('latent_space_2', 'Proteomic CN Aware Latent Space', **embedding_attrs_2)
+
+dot.edge('vae_contrastive_2', 'latent_space', label='Encodes to', minlen='2', labeldistance='2')
+# plate.edge('contrastive', 'latent_space', label='Refines', minlen='2', labeldistance='2')
+
+# dot.edge('latent_space', 'cn_pred')  # Invisible edge to control rank
 
 # Set rank for final node to be at the bottom
 with dot.subgraph() as bottom:
     bottom.attr(rank='sink')
-    bottom.node('cn_pred', 'Final Model for CN Prediction\n(Input: RNA, Output: CN)', **model_attrs)
-    dot.edge('plate_rep', 'cn_pred', label='',  minlen='2')
+    # bottom.node('cn_pred', 'Final Model for CN Prediction\n(Input: RNA, Output: CN)', **model_attrs)
+    dot.edge('plate_rep', 'vae_contrastive_2', label='Use Trained Encoder',  minlen='2')
     # dot.edge('cn_data', 'cn_pred', label=' Validate predictions\nwith ground truth CN labels', minlen='2', labeldistance='2')
 
 # Additional edges for contrastive learning context
